e5c74db4b370b2d575f3116166780188087f105d
shandohowell <shandohowell94@gmail.com>
2025-03-05 19:14:33 -0500

    Settng up image upload functionality.

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -1,7 +1,74 @@
 'use server';
 
-async function addProperty() {
-    console.log('addProperty action');
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/lib/getSessionUser";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import cloudinary from "@/config/cloudinary";
+
+async function addProperty(formData) {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+        throw new Error('User ID is required');
+    }
+
+    const { userId } = sessionUser;
+
+    // Access all values from amenities and images
+    const amenities = formData.getAll('amenities');
+    const images = formData
+        .getAll('images')
+        .filter((image) => image.name !== '')
+
+    const propertyData = {
+        owner: userId,
+        type: formData.get('type'),
+        name: formData.get('name'),
+        description: formData.get('description'),
+        location: {
+            street: formData.get('location.street'),
+            city: formData.get('location.city'),
+            state: formData.get('location.state'),
+            zipcode: formData.get('location.zipcode')
+        },
+        beds: formData.get('bed'),
+        baths: formData.get('baths'),
+        square_feet: formData.get('square_feet'),
+        amenities,
+        rates: {
+            nightly: formData.get('rates.nightly'),
+            weekly: formData.get('rates.weekly'),
+            monthly: formData.get('rates.monthly')
+            
+        },
+        seller_info: {
+            name: formData.get('seller_info.name'),
+            email: formData.get('seller_info.email'),
+            phone: formData.get('seller_info.phone')
+        },
+    };
+
+    const imageUrls = [];
+
+    for (const imageFile of images) {
+        const imageBuffer = await imageFile.arrayBuffer();
+        const imageArray = Array.from(new Uint8Array(imageBuffer));
+        const imageData = Buffer.from(imageArray);
+
+        // Convert to base64
+        const imageBase64 = imageData.toString('base64');
+    }
+
+    const newProperty = new Property(propertyData);
+    await newProperty.save();
+
+    revalidatePath('/', 'layout');
+
+    redirect(`/properties/${newProperty._id}`);
 }
 
 export default addProperty;
